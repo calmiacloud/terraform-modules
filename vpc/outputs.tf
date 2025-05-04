3438057e143bdc3ba7a7682feb181a71bf2734dd
edu0p92a6w169 <edu0p92-a6w169@pm.me>
2025-05-04 02:06:23 +0000

    test

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,19 +1,3 @@
 output "vpc_id" {
-  description = "ID de la VPC"
-  value       = aws_vpc.this.id
-}
-
-output "subnets_public_ids" {
-  description = "IDs de las subnets públicas"
-  value       = { for az, subnet in aws_subnet.public : az => subnet.id }
-}
-
-output "internet_gateway_id" {
-  description = "ID del Internet Gateway"
-  value       = aws_internet_gateway.this.id
-}
-
-output "route_table_id" {
-  description = "ID de la Route Table pública"
-  value       = aws_route_table.public.id
+  value = aws_vpc.this.id
 }
