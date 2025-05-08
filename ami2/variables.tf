6e9e01699210c836884f84ef916266c38aa390a2
edu0p92a6w169 <edu0p92-a6w169@pm.me>
2025-05-08 12:36:48 +0200

    Create variables.tf

---
+++ ami2/variables.tf
@@ -0,0 +1,31 @@
+variable "Name" {
+  type        = string
+}
+
+variable "Product" {
+  type        = string
+}
+
+variable "Environment" {
+  type        = string
+}
+
+variable "Playbook" {
+  type  = string
+}
+
+variable "Instance" {
+  type = object({
+    ParentImage    = string
+    InstanceType   = string
+    KeyPair        = string
+    Subnet         = string
+    SecurityGroup  = string
+
+  })
+}
+
+variable "ExtraVars" {
+  type    = any
+  default = {}
+}
