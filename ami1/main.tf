1230ec81d7f56276b69f0c7850525bd63774db41
edu0p92a6w169 <edu0p92-a6w169@pm.me>
2025-05-09 18:41:12 +0000

    test

--- ami1/main.tf
+++ ami1/main.tf
@@ -194,10 +194,10 @@ resource "aws_imagebuilder_image_recipe" "recipe_main" {
     component_arn = aws_image_builder_component.component_basicpackages.arn
   }
   component {
-    component_arn = aws_image_builder_component.component_ansible.arn
+    component_arn = "arn:aws:imagebuilder:eu-south-2:aws:component/aws-cli-version-2-linux/1.0.4/1"
   }
   component {
-    component_arn = "arn:aws:imagebuilder:eu-south-2:aws:component/aws-cli-version-2-linux/1.0.4/1"
+    component_arn = aws_image_builder_component.component_ansible.arn
   }
   component {
     component_arn = aws_image_builder_component.component_downloadplaybook.arn
