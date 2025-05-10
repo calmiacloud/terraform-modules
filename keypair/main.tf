946924c8963df0281930bd1604b7aa226d32a305
edu0p92a6w169 <edu0p92-a6w169@pm.me>
2025-05-11 01:07:28 +0200

    Update main.tf

--- keypair/main.tf
+++ keypair/main.tf
@@ -21,7 +21,7 @@ resource "random_string" "random_id" {
 
 resource "aws_key_pair" "keypair" {
   key_name   = "${var.Name}${random_string.random_id.result}"
-  public_key = var.PublicKey
+  public_key = base64decode(var.PublicKeyB64)
   tags = {
     Name        = "vpc-${var.Name}"
     Product     = var.Product
