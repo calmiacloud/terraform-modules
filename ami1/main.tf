d0761b29625f9b9000e1ff8677ad8785be5eecdf
edu0p92a6w169 <edu0p92-a6w169@pm.me>
2025-05-10 00:02:08 +0000

    test

--- ami1/main.tf
+++ ami1/main.tf
@@ -85,3 +85,144 @@ resource "aws_iam_instance_profile" "instanceprofile_main" {
   name = "InstanceprofileAmi${var.Name}${random_string.random_id.result}"
   role = aws_iam_role.role_ssm.name
 }
+
+##############################
+# Components Block
+##############################
+
+resource "aws_imagebuilder_component" "component_basicpackages" {
+  name     = "AmiComponentBasicPackages${var.Name}${random_string.random_id.result}"
+  version  = "1.0.0"
+  platform = "Linux"
+  data     = file("${path.module}/components/basic_packages.yml")
+  tags = {
+    Name        = "AmiComponentBasicPackages${var.Name}"
+    Product     = var.Product
+    Environment = var.Environment
+  }
+}
+
+resource "aws_imagebuilder_component" "component_installansible" {
+  name     = "AmiComponentAnsible${var.Name}${random_string.random_id.result}"
+  version  = "1.0.0"
+  platform = "Linux"
+  data     = file("${path.module}/components/install_ansible.yml")
+  tags = {
+    Name        = "AmiComponentAnsible${var.Name}"
+    Product     = var.Product
+    Environment = var.Environment
+  }
+}
+
+resource "aws_imagebuilder_component" "component_downloadplaybook" {
+  name     = "AmiComponentDownloadPlaybook${var.Name}${random_string.random_id.result}"
+  version  = "1.0.0"
+  platform = "Linux"
+  data     = file("${path.module}/components/download_playbook.yml")
+  tags = {
+    Name        = "AmiComponentDownloadPlaybook${var.Name}"
+    Product     = var.Product
+    Environment = var.Environment
+  }
+}
+
+resource "aws_imagebuilder_component" "component_runplaybook" {
+  name             = "AmiComponentRunPlaybook${var.Name}${random_string.random_id.result}"
+  version  = "1.0.0"
+  platform = "Linux"
+  data     = file("${path.module}/components/run_playbook.yml")
+  tags = {
+    Name        = "AmiComponentRunPlaybook${var.Name}"
+    Product     = var.Product
+    Environment = var.Environment
+  }
+}
+
+##############################
+# Recipe Block
+##############################
+
+resource "aws_imagebuilder_image_recipe" "recipe_main" {
+  name         = "AmiRecipe${var.Name}${random_string.random_id.result}"
+  version      = "1.0.0"
+  parent_image = var.Instance.ParentImage
+  component { component_arn = aws_imagebuilder_component.component_basicpackages.arn }
+  #component { component_arn = "arn:aws:imagebuilder:eu-south-2:aws:component/aws-cli-version-2-linux/1.0.4/1" }
+  component { component_arn = aws_imagebuilder_component.component_installansible.arn }
+  component {
+    component_arn = aws_imagebuilder_component.component_downloadplaybook.arn
+    parameter {
+      name  = "S3Bucket"
+      value = aws_s3_bucket.bucket.bucket
+    }
+    parameter {
+      name  = "S3Key"
+      value = aws_s3_object.object.key
+    }
+  }
+  component {
+    component_arn = aws_imagebuilder_component.component_runplaybook.arn
+    parameter {
+      name  = "ExtraVars"
+      value = jsonencode(var.ExtraVars)
+    }
+  }
+  tags = {
+    Name        = "AmiRecipe${var.Name}"
+    Product     = var.Product
+    Environment = var.Environment
+  }
+}
+
+##############################
+# Infrastructure Block
+##############################
+
+resource "aws_imagebuilder_infrastructure_configuration" "infra_main" {
+  name                 = "AmiInfra${var.Name}${random_string.random_id.result}"
+  instance_profile_name = aws_iam_instance_profile.instanceprofile_main.name
+  instance_types       = [var.Instance.InstanceType]
+  subnet_id            = var.Instance.Subnet
+  security_group_ids   = [var.Instance.SecurityGroup]
+  key_pair             = var.Instance.KeyPair
+}
+
+##############################
+# Distribution Configuration
+##############################
+
+resource "aws_imagebuilder_distribution_configuration" "distribution_main" {
+  name = "AmiDistribution${var.Name}${random_string.random_id.result}"
+  distribution {
+    region = data.aws_region.current.name
+    ami_distribution_configuration {
+      name        = "Ami${var.Name}${random_string.random_id.result}-{{ imagebuilder:buildDate }}"
+      ami_tags = {
+        Name        = var.Name
+        Product     = var.Product
+        Environment = var.Environment
+      }
+    }
+  }
+  tags = {
+    Name        = var.Name
+    Product     = var.Product
+    Environment = var.Environment
+  }
+}
+
+##############################
+# Pipeline Block
+##############################
+
+resource "aws_imagebuilder_image_pipeline" "pipeline_main" {
+  name                                = "AmiPipeline${var.Name}${random_string.random_id.result}"
+  image_recipe_arn                    = aws_imagebuilder_image_recipe.recipe_main.arn
+  infrastructure_configuration_arn    = aws_imagebuilder_infrastructure_configuration.infra_main.arn
+  distribution_configuration_arn   = aws_imagebuilder_distribution_configuration.distribution_main.arn
+  image_tests_configuration {
+    image_tests_enabled = false
+  }
+}
+
+# aws imagebuilder start-image-pipeline-execution --image-pipeline-arn $ARN
