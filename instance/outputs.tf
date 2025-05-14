3143ef8a64502d28ada50ec15a8c1cedc8c1aa08
edu0p92a6w169 <edu0p92-a6w169@pm.me>
2025-05-14 00:47:44 +0000

    test

--- instance/outputs.tf
+++ instance/outputs.tf
@@ -1,24 +1,3 @@
 output "Id" {
-  value = aws_vpc.vpc.id
-}
-
-output "SubnetPublic" {
-  value = {
-    for idx in range(length(var.Subnets.Public)) :
-    var.Subnets.Public[idx].Name => aws_subnet.subnet_public[idx].id
-  }
-}
-
-output "SubnetNat" {
-  value = {
-    for idx in range(length(var.Subnets.Nat)) :
-    var.Subnets.Nat[idx].Name => aws_subnet.subnet_nat[idx].id
-  }
-}
-
-output "SubnetPrivate" {
-  value = {
-    for name, subnet in aws_subnet.subnet_private :
-    name => subnet.id
-  }
+  value = aws_instance.instance.id
 }
