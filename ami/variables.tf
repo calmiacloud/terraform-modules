c99a76116f3567562e3e739214b01d3e856842fa
edu0p92a6w169 <edu0p92-a6w169@pm.me>
2025-05-25 03:30:51 +0200

    Update variables.tf

--- ami/variables.tf
+++ ami/variables.tf
@@ -2,14 +2,6 @@ variable "Name" {
   type        = string
 }
 
-variable "tags" {
-  type = object({
-    Project = string
-    Repo    = string
-    Stage   = string
-  })
-}
-
 variable "Source" {
   type  = string
 }
