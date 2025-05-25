f8f466c964ebf0aa6d594fb8d4084a78dfc8d830
edu0p92a6w169 <edu0p92-a6w169@pm.me>
2025-05-25 03:31:16 +0200

    Update variables.tf

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -2,14 +2,6 @@ variable "Name" {
   type        = string
 }
 
-variable "tags" {
-  type = object({
-    Project = string
-    Repo    = string
-    Stage   = string
-  })
-}
-
 variable "Vpc" {
   type = object({
     Cidr        = string
