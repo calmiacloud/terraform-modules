33cb9951a73ae72a05a835c110303d0da7ad1a87
edu0p92a6w169 <edu0p92-a6w169@pm.me>
2025-05-25 03:26:33 +0200

    Update main.tf

--- keypair/main.tf
+++ keypair/main.tf
@@ -5,9 +5,10 @@
 resource "aws_key_pair" "keypair" {
   key_name   = "${var.Product}${var.Stage}${var.Name}"
   public_key = base64decode(var.PublicKeyB64)
-  tags = {
-    Name        = var.Name
-    Product     = var.Product
-    Stage       = var.Stage
-  }
+  tags = merge(
+    {
+      Name = var.Name
+    },
+    var.tags
+  )
 }
