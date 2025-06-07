92df2cd140a50334729f4f4b9d8ddab86c4f40ff
edu0p92 <edu0p92@pm.me>
2025-06-07 20:09:45 +0000

    s

--- route53zone/main.tf
+++ route53zone/main.tf
@@ -5,14 +5,14 @@
 resource "aws_route53_zone" "zone" {
   name = var.domain
   dynamic "vpc" {
-    for_each = var.is_private ? var.vpc_ids : []
+    for_each = var.Type == "Private" ? var.Vpc : []
     content {
       vpc_id = vpc.value
     }
   }
-  private_zone = var.is_private
+  private_zone = var.Type == "Private"
   tags = merge(var.Tags, {
-    Name = "Royte53Zone${var.Name}"
+    Name = "Route53Zone${var.Name}"
   })
 }
 
--- route53zone/variables.tf
+++ route53zone/variables.tf
@@ -6,6 +6,22 @@ variable "Domain" {
   type        = string
 }
 
+variable "Type" {
+  type        = string
+  validation {
+    condition     = var.Type == "Public" || var.Type == "Private"
+    error_message = "El tipo debe ser 'Public' o 'Private'."
+  }
+}
+
+variable "Vpc" {
+  type        = string
+  validation {
+    condition = var.Type == "Public" || (var.Type == "Private" && length(var.vpc_ids) > 0)
+    error_message = "Debe especificarse al menos una VPC si el tipo de zona es 'Private'."
+  }
+}
+
 variable "Tags" {
   type = object({
     Project = string
