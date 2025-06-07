bc2372b51a1271fb4b8bc7e7ea1ce697f941ee22
edu0p92 <edu0p92@pm.me>
2025-06-07 20:10:24 +0000

    s

--- route53zone/outputs.tf
+++ route53zone/outputs.tf
@@ -1,24 +1,8 @@
 output "Id" {
-  value = aws_vpc.vpc.id
+  value       = aws_route53_zone.zone.zone_id
 }
 
-output "SubnetPublic" {
-  value = {
-    for idx in range(length(var.Subnets.Public)) :
-    var.Subnets.Public[idx].Name => aws_subnet.subnet_public[idx].id
-  }
-}
-
-output "SubnetNat" {
-  value = {
-    for idx in range(length(var.Subnets.Nat)) :
-    var.Subnets.Nat[idx].Name => aws_subnet.subnet_nat[idx].id
-  }
-}
-
-output "SubnetPrivate" {
-  value = {
-    for name, subnet in aws_subnet.subnet_private :
-    name => subnet.id
-  }
+output "Nameservers" {
+  value       = aws_route53_zone.zone.name_servers
+  condition   = var.Type == "Public"
 }
