e248eee1de62333af88d72b25e6db70b34f06167
edu0p92 <edu0p92@pm.me>
2025-06-07 20:25:28 +0000

    d

--- route53zone/main.tf
+++ route53zone/main.tf
@@ -5,7 +5,7 @@
 resource "aws_route53_zone" "zone" {
   name = var.domain
   dynamic "vpc" {
-    for_each = var.Type == "Private" ? var.Vpc : []
+    for_each = var.Type == "Private" ? [var.Vpc] : []
     content {
       vpc_id = vpc.value
     }
