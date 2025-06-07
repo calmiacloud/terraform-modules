40c9f8d024d7261571fca049cc11d6aedc56e613
edu0p92 <edu0p92@pm.me>
2025-06-07 23:34:58 +0000

    t

--- route53record/main.tf
+++ route53record/main.tf
@@ -2,20 +2,6 @@
 # Route53 Block
 ##############################
 
-resource "aws_route53_zone" "zone" {
-  name = var.Domain
-  dynamic "vpc" {
-    for_each = var.Type == "Private" ? [var.Vpc] : []
-    content {
-      vpc_id = vpc.value
-    }
-  }
-  tags = merge(var.Tags, {
-    Name = "Route53Zone${var.Name}"
-  })
-}
-
-
 resource "aws_route53_record" "record" {
   for_each = {
     for Record in var.Records : "${Record.Name}-${Record.Type}" => record
