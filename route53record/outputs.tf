ba4d140a364161dc453c5dcbc249cc3fcfd9efe0
edu0p92 <edu0p92@pm.me>
2025-06-07 23:36:10 +0000

    k

--- route53record/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "Id" {
-  value       = aws_route53_zone.zone.zone_id
-}
-
-output "Nameservers" {
-  value       = var.Type == "Public" ? aws_route53_zone.zone.name_servers : []
-}
--- route53record/variables.tf
+++ route53record/variables.tf
@@ -1,27 +1,12 @@
-variable "Name" {
+variable "Zone" {
   type        = string
 }
 
-variable "Domain" {
-  type        = string
-}
-
-variable "Type" {
-  type        = string
-  validation {
-    condition     = var.Type == "Public" || var.Type == "Private"
-    error_message = "El tipo debe ser 'Public' o 'Private'."
-  }
-}
-
-variable "Vpc" {
-  type        = string
-}
-
-variable "Tags" {
-  type = object({
-    Project = string
-    Repo    = string
-    Stage   = string
-  })
+variable "Records" {
+  type = list(object({
+    Name    = string
+    Type    = string            # A, CNAME, MX
+    Ttl     = number
+    Records = any               # lista de strings para A/CNAME, lista de objetos para MX
+  }))
 }
