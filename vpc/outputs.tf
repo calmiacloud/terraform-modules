86d69ce0cfa1c00812f68f906947efbaa75a9def
edu0p92 <edu0p92@pm.me>
2025-06-09 21:24:12 +0000

    G

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -4,27 +4,30 @@ output "Id" {
 
 output "SubnetPublic" {
   value = {
-    for idx, cidr in flatten([for name, cidrs in var.Subnets.Public : [
-      for i, c in cidrs : "${name}${i}"
-    ]]) :
-    cidr => aws_subnet.subnet_public[idx].id
+    for name, cidrs in var.Subnets.Public :
+    name => [
+      for i in range(length(cidrs)) :
+      aws_subnet.subnet_public[sum([for n, c in var.Subnets.Public : n == name ? length(c) : 0]) - length(cidrs) + i].id
+    ]
   }
 }
 
 output "SubnetNat" {
   value = {
-    for idx, cidr in flatten([for name, cidrs in lookup(var.Subnets, "Nat", {}) : [
-      for i, c in cidrs : "${name}${i}"
-    ]]) :
-    cidr => aws_subnet.subnet_nat[idx].id
+    for name, cidrs in lookup(var.Subnets, "Nat", {}) :
+    name => [
+      for i in range(length(cidrs)) :
+      aws_subnet.subnet_nat[sum([for n, c in lookup(var.Subnets, "Nat", {}) : n == name ? length(c) : 0]) - length(cidrs) + i].id
+    ]
   }
 }
 
 output "SubnetPrivate" {
   value = {
-    for idx, cidr in flatten([for name, cidrs in lookup(var.Subnets, "Private", {}) : [
-      for i, c in cidrs : "${name}${i}"
-    ]]) :
-    cidr => aws_subnet.subnet_private[idx].id
+    for name, cidrs in lookup(var.Subnets, "Private", {}) :
+    name => [
+      for i in range(length(cidrs)) :
+      aws_subnet.subnet_private[sum([for n, c in lookup(var.Subnets, "Private", {}) : n == name ? length(c) : 0]) - length(cidrs) + i].id
+    ]
   }
 }
