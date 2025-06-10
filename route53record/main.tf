bb0281fde2bc21cd5b83573244abcb8fdaad73f3
edu0p92 <edu0p92@pm.me>
2025-06-10 21:22:07 +0200

    Update main.tf

--- route53record/main.tf
+++ route53record/main.tf
@@ -18,13 +18,14 @@ resource "aws_route53_record" "record" {
 # Waiter Block
 ##############################
 
-resource "null_resource" "resource_main" {
-  depends_on = [aws_route53_record.record]
-
+resource "null_resource" "wait_record" {
   for_each = {
-    for record in var.Records : "${record.Name}-${record.Type}" => record
+    for record in var.Records :
+    "${record.Name}-${record.Type}" => record
   }
-
+  depends_on = [
+    aws_route53_record.record[each.key]
+  ]
   triggers = {
     name  = each.value.Name
     type  = each.value.Type
@@ -33,17 +34,10 @@ resource "null_resource" "resource_main" {
       : join(",", each.value.Values)
     zone  = var.Zone
   }
-
   provisioner "local-exec" {
     environment = {
       TF_ACTION = terraform.workspace == "destroy" ? "destroy" : "apply"
     }
-    command = <<EOT
-      bash ${path.module}/src/checkrecord.sh \
-        "${self.triggers.name}" \
-        "${self.triggers.type}" \
-        "${self.triggers.value}" \
-        "${self.triggers.zone}"
-    EOT
+    command = bash ${path.module}/src/checkrecord.sh ${self.triggers.name} ${self.triggers.type} ${self.triggers.value} ${self.triggers.zone}"
   }
 }
