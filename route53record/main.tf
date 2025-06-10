106ee710770418c057a0387f285bc1277ca34a20
edu0p92 <edu0p92@pm.me>
2025-06-10 20:52:34 +0200

    Update main.tf

--- route53record/main.tf
+++ route53record/main.tf
@@ -9,9 +9,10 @@ resource "aws_route53_record" "record" {
   name    = each.value.Name
   type    = each.value.Type
   ttl     = each.value.Ttl
-  records = each.value.Records
+  records = each.value.Values   # aquí apuntamos a Values
   zone_id = var.Zone
 }
+
 
 ##############################
 # Waiter Block
@@ -19,15 +20,18 @@ resource "aws_route53_record" "record" {
 
 resource "null_resource" "resource_main" {
   depends_on = [aws_route53_record.record]
+
   for_each = {
     for record in var.Records : "${record.Name}-${record.Type}" => record
   }
+
   triggers = {
-    name   = each.value.Name
-    type   = each.value.Type
-    value  = join(",", each.value.Records)
-    zone   = var.Zone
+    name  = each.value.Name
+    type  = each.value.Type
+    value = join(",", each.value.Values)
+    zone  = var.Zone
   }
+
   provisioner "local-exec" {
     environment = {
       TF_ACTION = terraform.workspace == "destroy" ? "destroy" : "apply"
