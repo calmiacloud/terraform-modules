842a793ad2687ad375d05fdcd736f7db0ffe7fd6
edu0p92 <edu0p92@pm.me>
2025-06-10 00:28:55 +0000

    s

--- sesidentity/main.tf
+++ sesidentity/main.tf
@@ -5,3 +5,7 @@
 resource "aws_ses_domain_identity" "identity" {
   domain = var.Domain
 }
+
+resource "aws_ses_domain_dkim" "dkim" {
+  domain = aws_ses_domain_identity.identity.domain
+}
--- sesidentity/outputs.tf
+++ sesidentity/outputs.tf
@@ -5,3 +5,7 @@ output "Arn" {
 output "VerificationToken" {
   value       = aws_ses_domain_identity.identity.verification_token
 }
+
+output "DkimTokens" {
+  value       = aws_ses_domain_dkim.dkim.dkim_tokens
+}
