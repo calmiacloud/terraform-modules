c526fcfcaf7bcf1ea1c34a69b3bd0af0b7175741
edu0p92 <edu0p92@pm.me>
2025-06-10 20:52:36 +0200

    Update variables.tf

--- route53record/variables.tf
+++ route53record/variables.tf
@@ -3,10 +3,11 @@ variable "Zone" {
 }
 
 variable "Records" {
+  description = "Lista de definiciones de registros Route53"
   type = list(object({
-    Name    = string
-    Type    = string            # A, CNAME, MX
-    Ttl     = number
-    Values = any               # lista de strings para A/CNAME, lista de objetos para MX
+    Name   = string
+    Type   = string
+    Ttl    = number
+    Values = list(string)     # antes era Records = list(string)
   }))
 }
