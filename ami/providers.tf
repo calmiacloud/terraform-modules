306b1ed20ee891febb6b3aec1f19628b6ebc08a8
calmiacloud <calmiacloud@gmail.com>
2025-09-22 20:27:45 +0000

    Update

---
+++ ami/providers.tf
@@ -0,0 +1,12 @@
+#################################
+# Providers Block
+##################################
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
